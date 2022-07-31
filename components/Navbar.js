77886b2938722e40a2d76e9efcdbc665a45e00c8
therealbeez <gabriel.agbese2@gmail.com>
2022-07-31 15:37:49 +0200

    basic menu functionality

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,34 @@
 export default function Navbar () {
+
+    
+
     return (
         <div className="navbar">
-            <h1>Navbar</h1>
+            <div className="topnav responsive-nav" id="myTopnav">
+                <div className="nav-main">
+                    <a href="#home" class="active">Home</a>
+                    <a href="#news">News</a>
+                    <a href="#contact">Contact</a>
+                    <a>About</a>
+                </div>
+                <div className="nav-mobile">
+                    <a className="menu-button" href="#menu" onClick={() => toggleMenu()}>Menu</a>
+                    <div id="dropdown">
+                        <a href="#">Link 1</a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
+
+
+const toggleMenu = () => {
+    console.log("clicked");
+    var x = document.getElementById("dropdown");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
